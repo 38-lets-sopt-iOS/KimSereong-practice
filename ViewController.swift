c66af9231fbb50054bfc332afa1bf9186160f66d
ser0kim <srkim0728@gmail.com>
2026-04-10 21:08:42 +0900

    당근마켓 로그인 UI 구현

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LETSSOPT_iOS_Seminar_review
+//
+//  Created by 김세령 on 4/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
